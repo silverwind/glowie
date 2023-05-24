0187a4a23285b9e074465bade0573fd1233760c3
silverwind <me@silverwind.io>
2023-05-24 08:40:12 +0200

    move file

--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-import {red, disableColor, enableColor} from "./glowie.js";
+import {red, disableColor, enableColor} from "./index.js";
 
 test("test", () => {
   expect(red("foo")).toEqual("\u001b[31mfoo\u001b[39m");
